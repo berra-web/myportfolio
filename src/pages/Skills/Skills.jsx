5213e91fd6f01ab592623a3143a34b60f9869a7f
Behrad <hbberra@gmail.com>
2021-04-30 19:05:19 +0200

    responsive-2

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Skills.css';
-import {Col, Row,Button } from 'react-bootstrap';
+import {Container, Col, Row,Button } from 'react-bootstrap';
 import {skills} from './SkillsData.js'; 
 import Barbarian from './Barbarian/Barbarian';
 import { Link } from 'react-router-dom';
@@ -14,7 +14,7 @@ export default function Skills() {
       });
 
     return (
-        <div className='skill-container' >
+        <Container className='skill-container' fluid>
             <Row>
                 <Col className='col-services'>
                   <h1 className="mainHeader">{header.subHeading}</h1>
@@ -45,6 +45,6 @@ export default function Skills() {
                 </Col>
               <p><Nav.Link as={Link} to="/contact"><Button variant="info" className='service-button'>Get Starded</Button></Nav.Link></p>
             </div>
-        </div>
+        </Container>
     )
 }
